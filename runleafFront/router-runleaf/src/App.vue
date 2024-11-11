ecb5f979c42b30db5146ec88f040be178490906b
Jayden <lsu0930@gmail.com>
2024-11-11 22:07:50 +0900

    11-11

--- runleafFront/router-runleaf/src/App.vue
+++ runleafFront/router-runleaf/src/App.vue
@@ -2,6 +2,8 @@
   <div>
       <RouterLink :to="{ name: 'gps'}">gps</RouterLink>
       <RouterLink :to="{ name: 'gps'}">gps</RouterLink>
+      <RouterLink :to="{ name: 'join'}">회원가입</RouterLink>
+
   </div>
   <RouterView />
 </template>
--- runleafFront/router-runleaf/src/router/index.js
+++ runleafFront/router-runleaf/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import GpsView from '@/views/GpsView.vue'
+import JoinView from '@/views/JoinView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,7 +10,11 @@ const router = createRouter({
       name: 'gps',
       component: GpsView,
     },
-
+    {
+      path: '/join',
+      name: 'join',
+      component: JoinView,
+    },
   ],
 })
 
